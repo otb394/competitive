4dd9c324735ae774f2571840077c3a397993671d
Rishabh Agrawal <agrawar@amazon.com>
2019-04-25 00:27:57 +0530

    Adding new methods to MathUtility

--- src/util/MathUtility.java
+++ src/util/MathUtility.java
@@ -1,5 +1,10 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class MathUtility {
     public static int pow(int b, long p, int MOD) {
         int ret = 1;
@@ -36,5 +41,71 @@ public class MathUtility {
     public static boolean isSquare(int n) {
         int sq = (int) Math.sqrt(n);
         return ((sq * sq) == n);
+    }
+
+    public static int hcf(int a, int b) {
+        if (a==0) {
+            return b;
+        }
+        return hcf(b%a, a);
+    }
+
+    public static int hcf(int[] arr, int n) {
+        int result = arr[0];
+        for(int i=1;i<n;i++) {
+            result=hcf(arr[i],result);
+        }
+        return result;
+    }
+
+    public static long hcf(long a, long b) {
+        if (a==0L) {
+            return b;
+        }
+        return hcf(b%a, a);
+    }
+
+    public static long hcf(long[] arr, int n) {
+        long result = arr[0];
+        for(int i=1;i<n;i++) {
+            result=hcf(arr[i],result);
+        }
+        return result;
+    }
+
+    public static long lcm(long a, long b) {
+        if (a>b) return lcm(b,a);
+        long hcf = hcf(a,b);
+        return (b/hcf)*a;
+    }
+
+    /**
+     * Evaluates all the factors of a given number.
+     *
+     * No repetitions. Output is in no specific order.
+     * Note: May need optimization.
+     *
+     * @param n Given number
+     * @return All factors of n
+     */
+    public static int[] factorize(int n) {
+        if (n==0) return new int[0];
+        int s = (int) Math.floor(Math.sqrt(n));
+        Set<Integer> factors = new HashSet<>();
+        factors.add(1);
+        factors.add(n);
+        for (int i=2;i<=s;i++) {
+            if ((n%i)==0) {
+                factors.add(i);factors.add(n/i);
+            }
+        }
+        return factors.stream().mapToInt(i -> i).toArray();
+    }
+
+    /**
+     * Checks whether the given number is a power of two or not.
+     */
+    public static boolean isPowerOfTwo(long n) {
+        return n == (Long.highestOneBit(n));
     }
 }
